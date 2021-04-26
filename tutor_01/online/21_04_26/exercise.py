f9eeee890fe58ff2f5cfcb019de129704c73b8a1
EumYoungHyun <opwer032@naver.com>
2021-04-27 00:10:15 +0900

    feat: Learn Array append function

--- tutor_01/online/21_04_26/exercise.py
+++ tutor_01/online/21_04_26/exercise.py
@@ -1,31 +1,45 @@
 
-# 1번문제
-"""
-    배열 함수 이용법 익히는 문제같음
-"""
-# 1-1
-lst = [10, 1, 5, 2]
+# # 1번문제
+# """
+#     배열 함수 이용법 익히는 문제같음
+# """
+# # 1-1
+# lst = [10, 1, 5, 2]
 
-# 초기화
-result = []
-# 배열을 뒤에 합치는 함수 extend
-result.extend(lst)
-result.extend(lst)
+# # 초기화
+# result = []
+# # 배열을 뒤에 합치는 함수 extend
+# result.extend(lst)
+# result.extend(lst)
 
-print(result) # 출력: [10, 1, 5, 2, 10, 1, 5, 2]
+# print(result) # 출력: [10, 1, 5, 2, 10, 1, 5, 2]
 
-# 1-2
-result.append(result[0]*2)
+# # 1-2
+# result.append(result[0]*2)
 
-print(result) # 출력: [10, 1, 5, 2, 10, 1, 5, 2, 20]
+# print(result) # 출력: [10, 1, 5, 2, 10, 1, 5, 2, 20]
 
-# 1-3
-# 홀수 체크 
-"""
-    list compregension 
-    리스트의 특정값들을 여과하는 작업
-"""
-result = result[1::2] # 1번째 항목부터 index를 2칸씩 이동
+# # 1-3
+# # 홀수 체크 
+# """
+#     list compregension 
+#     리스트의 특정값들을 여과하는 작업
+# """
+# result = result[1::2] # 1번째 항목부터 index를 2칸씩 이동
 
-print(result)
+# print(result)
 
+
+
+# 2번 문제
+```
+    배열 함수 append 활용
+```
+vectorArray = []
+vectorArrayLength = int(input("vertor 수: "))
+
+for i in range(0, vectorArrayLength):
+    vector=input()
+    vectorArray.append(vector)
+
+print("vector 크기: " + str(len(vectorArray)))
