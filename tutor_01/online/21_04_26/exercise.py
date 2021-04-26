63349049f79288008f91e664aa46a20e5ba92d35
Eum <opwer032@naver.com>
2021-04-26 13:44:13 +0900

    refactor: Adjust file directory

---
+++ tutor_01/online/21_04_26/exercise.py
@@ -0,0 +1,31 @@
+
+# 1번문제
+"""
+    배열 함수 이용법 익히는 문제같음
+"""
+# 1-1
+lst = [10, 1, 5, 2]
+
+# 초기화
+result = []
+# 배열을 뒤에 합치는 함수 extend
+result.extend(lst)
+result.extend(lst)
+
+print(result) # 출력: [10, 1, 5, 2, 10, 1, 5, 2]
+
+# 1-2
+result.append(result[0]*2)
+
+print(result) # 출력: [10, 1, 5, 2, 10, 1, 5, 2, 20]
+
+# 1-3
+# 홀수 체크 
+"""
+    list compregension 
+    리스트의 특정값들을 여과하는 작업
+"""
+result = result[1::2] # 1번째 항목부터 index를 2칸씩 이동
+
+print(result)
+
