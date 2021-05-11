8af6b745492129ae1eaa7ac039fcc11e70792ca8
Eum <opwer032@naver.com>
2021-05-11 18:42:02 +0900

    crawl website and use numpy

--- tutor_01/online/21_04_26/exercise.py
+++ tutor_01/online/21_04_26/exercise.py
@@ -21,11 +21,49 @@ print(result) # 출력: [10, 1, 5, 2, 10, 1, 5, 2, 20]
 
 # 1-3
 # 홀수 체크 
-"""
-    list compregension 
-    리스트의 특정값들을 여과하는 작업
-"""
-result = result[1::2] # 1번째 항목부터 index를 2칸씩 이동
+# """
+#     list compregension 
+#     리스트의 특정값들을 여과하는 작업
+# """
+# result = result[1::2] # 1번째 항목부터 index를 2칸씩 이동
 
-print(result)
+# print(result)
 
+
+class Expense:
+    def __init__(self, date, coffee, traffic, bob):
+        self.date = date
+        self.coffee = coffee
+        self.traffic = traffic
+        self.bob = bob
+
+    def get_sum(self):
+        return self.coffee + self.traffic + self.bob
+
+    def get_string(self):
+        return "{}\t{}\t{}\t{}\t{}".format(
+            self.date,
+            self.coffee,
+            self.traffic,
+            self.bob,
+            self.get_sum())
+
+expenses = [
+    Expense("3/1", 8700, 9800, 8800),
+    Expense("3/2", 5700, 9700, 6700),
+    Expense("3/3", 8700, 9800, 9800),
+    Expense("3/4", 5700, 9500, 6700),
+    Expense("3/5", 8700, 6800, 8800),
+    Expense("3/6", 5700, 9700, 6700),
+    Expense("3/7", 8300, 9800, 9800),
+    Expense("3/8", 5700, 9500, 9700)
+]
+
+print("일자","음료대","교통비","기타비용","합계")
+
+coffee_total_price = 0
+for expense in expenses:
+    print(expense.get_string())
+    coffee_total_price = coffee_total_price + expense.coffee
+
+print(coffee_total_price)
