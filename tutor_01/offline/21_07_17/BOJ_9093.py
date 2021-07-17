ffbd732acbbde8407d037e3668a1c546661d082b
EumYoungHyun <opwer032@naver.com>
2021-07-17 13:20:38 +0900

    feat: Solve algorithm boj 9093

--- tutor_01/offline/21_07_17/BOJ_9093.py
+++ tutor_01/offline/21_07_17/BOJ_9093.py
@@ -1,7 +1,15 @@
+# N = int(input())
+
+# for i in range (N):
+#     str_array = input().split(" ")
+#     for word in str_array:
+#         print(word[::-1], end=" ")
+#     print()
+
 N = int(input())
 
 for i in range (N):
     str_array = input().split(" ")
     for word in str_array:
-        print(word[::-1], end=" ")
+        print("".join(reversed(word)), end=" ")
     print()
