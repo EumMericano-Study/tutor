e98a501956f9f11dc45f0166bcebfdd59eaa58e9
Eum Young Hyun <opwer032@naver.com>
2021-09-19 18:22:48 +0900

    feat: Set login logic

--- tutor_01/offline/21_09_19/tutor01/src/pages/login.jsx
+++ tutor_01/offline/21_09_19/tutor01/src/pages/login.jsx
@@ -2,20 +2,50 @@ import React from "react";
 import styled from "styled-components";
 
 class Login extends React.Component {
+  state = {
+    id: "",
+    password: "",
+  };
+
+  handleInput = (e) => {
+    const { name, value } = e.target;
+    if (name === "id") this.setState({ id: value });
+    if (name === "password") this.setState({ password: value });
+  };
+
+  handleSubmit = () => {
+    const { id, password } = this.state;
+    if (id === "opwer032" && password === "password") {
+      window.location.href = "/home";
+    }
+  };
+
   render() {
     return (
       <Container>
         <LoginBox>
           <Title>로그인</Title>
           <InputContainer>
-            <input type="text" />
-            <input type="text" />
+            <input
+              type="text"
+              placeholder="id를 입력해주세요"
+              name="id"
+              value={this.state.id}
+              onChange={this.handleInput}
+            />
+            <input
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInput}
+            />
             <CheckContainer>
               <input type="checkbox" />
               <h4>로그인 상태 유지</h4>
             </CheckContainer>
           </InputContainer>
-          <button>-)</button>
+          <button onClick={this.handleSubmit}>-)</button>
         </LoginBox>
       </Container>
     );
@@ -30,8 +60,6 @@ const Container = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-
-  border: 5px solid blue;
 `;
 
 const LoginBox = styled.div`
@@ -43,8 +71,11 @@ const LoginBox = styled.div`
 `;
 
 const Title = styled.h2`
+  font-family: "Black Han Sans", sans-serif;
+  font-size: 3rem;
   text-align: center;
-  margin-bottom: 2em;
+
+  margin-bottom: 1em;
 `;
 
 const InputContainer = styled.div`
