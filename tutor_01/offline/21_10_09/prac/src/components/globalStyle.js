5ace5117ee99fc46bdea9cd8f457f2ba97b2c9b8
Eum Young Hyun <opwer032@naver.com>
2021-10-09 16:37:07 +0900

    feat: Add footer

--- tutor_01/offline/21_10_09/prac/src/components/globalStyle.js
+++ tutor_01/offline/21_10_09/prac/src/components/globalStyle.js
@@ -4,9 +4,9 @@ import reset from "styled-reset";
 const GlobalStyle = createGlobalStyle`
     ${reset}
     body {
-        min-height: 100vh;
         box-sizing: border-box;
     }
+
 `;
 
 export default GlobalStyle;
--- tutor_01/offline/21_10_09/prac/src/pages/login/index.js
+++ tutor_01/offline/21_10_09/prac/src/pages/login/index.js
@@ -2,13 +2,35 @@ import React from "react";
 import styled from "styled-components";
 
 function Login() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <BlockAlign></BlockAlign>
+      {/* <Block></Block>
+      <Block></Block> */}
+    </Container>
+  );
 }
 export default Login;
 
 const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 85vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-  border: 3px solid orange;
+const BlockAlign = styled.div`
+  width: 100%;
+  max-width: 800px;
+  height: 85%;
+
+  display: flex;
+  border: 1px solid red;
+`;
+
+const Block = styled.div`
+  width: 50%;
+  height: 100%;
+  border: 1px solid black;
 `;
