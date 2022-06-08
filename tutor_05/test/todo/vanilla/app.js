b6a96462522e2138c96e421897efba1ddaf79f54
Eumericano <opwer032@naver.com>
2022-06-08 09:50:22 +0900

    feat: Add draw function

--- tutor_05/test/todo/vanilla/app.js
+++ tutor_05/test/todo/vanilla/app.js
@@ -1,3 +1,33 @@
-const init = () => {};
+const todos_ul = document.querySelector(".todo-list");
+const TODOS_LOCAL_STORAGE = "TODOS_LOCAL_STORAGE";
+
+let todos = [
+  { index: 1, content: "코딩", checked: false, createdAt: new Date() },
+  { index: 2, content: "아이디어 기획", checked: false, createdAt: new Date() },
+  { index: 3, content: "손 씻기", checked: false, createdAt: new Date() },
+];
+
+const draw = () => {
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    li.className = "todo-thing";
+    li.dataset.index = todo.index;
+
+    const content = document.createElement("h2");
+    content.innerText = todo.content;
+
+    li.appendChild(content);
+    todos_ul.appendChild(li);
+  });
+};
+
+const checkTodo = () => {
+  return localStorage.getItem(TODOS_LOCAL_STORAGE) || [];
+};
+
+const init = () => {
+  checkTodo();
+  draw();
+};
 
 init();
