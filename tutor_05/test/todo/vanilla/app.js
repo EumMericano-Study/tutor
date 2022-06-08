152044e3a16b690ba6ea8225aaf1ba8c4b8e87e1
Eum Young Hyun <opwer032@naver.com>
2022-06-09 00:29:11 +0900

    feat: li 요소 추가

--- tutor_05/test/todo/vanilla/app.js
+++ tutor_05/test/todo/vanilla/app.js
@@ -7,19 +7,31 @@ let todos = [
   { index: 3, content: "손 씻기", checked: false, createdAt: new Date() },
 ];
 
-const draw = () => {
-  todos.forEach((todo) => {
-    const li = document.createElement("li");
-    li.className = "todo-thing";
-    li.dataset.index = todo.index;
+const makeTodo = (todo) => {
+  const li = document.createElement("li");
+  li.className = "todo-thing";
+  li.dataset.index = todo.index;
 
-    const content = document.createElement("h2");
-    content.innerText = todo.content;
+  const content = document.createElement("h2");
+  content.innerText = todo.content;
+  li.classList = "content";
 
-    li.appendChild(content);
-    todos_ul.appendChild(li);
-  });
+  const checkBtn = document.createElement("button");
+  checkBtn.innerText = "✔";
+  li.classList = "check";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "🗑";
+  li.classList = "remove";
+
+  li.append(checkBtn);
+  li.append(content);
+  li.append(deleteBtn);
+  return li;
 };
+
+const draw = () =>
+  todos.forEach((todo) => todos_ul.appendChild(makeTodo(todo)));
 
 const checkTodo = () => {
   return localStorage.getItem(TODOS_LOCAL_STORAGE) || [];
