4b688e7010a18c5521e9444f11e3203c89ed2206
Eumericano <opwer032@naver.com>
2022-06-10 19:47:15 +0900

    feat: styled component 기본 설정

---
+++ tutor_05/test/todo/react-ver/src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+function App() {
+  return <Container>hi</Container>;
+}
+
+export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  border: 1px solid red;
+`;
---
+++ tutor_05/test/todo/react-ver/src/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%
+  }
+`;
+
+export default GlobalStyle;
